34874217783fcaf366fe12d57774823aa3df8dd2
Parth Dave <parth.dave@nexuslinkservices.in>
2023-01-16 18:57:52 +0530

    added gateway basic and updated docker config

---
+++ gateway/index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+const expressProxy = require("express-proxy");
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/cart", expressProxy("http://localhost:3001"));
+app.use("/dashboard", expressProxy("http://localhost:3002"));
+app.use("/help", expressProxy("http://localhost:3003"));
+app.use("/payment", expressProxy("http://localhost:3004"));
+app.use("/profile", expressProxy("http://localhost:3005"));
+
+app.listen(3000, () => {
+  console.log("Gateway running on port 3000");
+});
