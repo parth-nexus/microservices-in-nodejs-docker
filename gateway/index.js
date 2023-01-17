32207f4efa42c5187fcf370ee7edf94ae9398879
Parth Dave <parth.dave@nexuslinkservices.in>
2023-01-17 17:10:50 +0530

    updated ip address as per docker

--- gateway/index.js
+++ gateway/index.js
@@ -2,20 +2,20 @@ const express = require("express");
 const app = express();
 const Proxy = require("express-http-proxy");
 
-app.get("/cart", Proxy("http://localhost:3001"));
-app.post("/add-to-cart", Proxy("http://localhost:3001"));
+app.get("/cart", Proxy("http://172.21.0.3:3001"));
+app.post("/add-to-cart", Proxy("http://172.21.0.3:3001"));
 
-app.get("/dashboard", Proxy("http://localhost:3002"));
-app.post("/dashboard", Proxy("http://localhost:3002"));
+app.get("/dashboard", Proxy("http://172.21.0.4:3002"));
+app.post("/dashboard", Proxy("http://172.21.0.4:3002"));
 
-app.use("/help", Proxy("http://localhost:3003"));
-app.post("/help", Proxy("http://localhost:3003"));
+app.use("/help", Proxy("http://172.21.0.5:3003"));
+app.post("/help", Proxy("http://172.21.0.5:3003"));
 
-app.use("/payment", Proxy("http://localhost:3004"));
-app.post("/payment", Proxy("http://localhost:3004"));
+app.use("/payment", Proxy("http://172.21.0.6:3004"));
+app.post("/payment", Proxy("http://172.21.0.6:3004"));
 
-app.use("/profile", Proxy("http://localhost:3005"));
-app.post("/profile", Proxy("http://localhost:3005"));
+app.use("/profile", Proxy("http://172.21.0.7:3005"));
+app.post("/profile", Proxy("http://172.21.0.7:3005"));
 
 app.listen(3000, () => {
   console.log("Gateway running on port 3000");
